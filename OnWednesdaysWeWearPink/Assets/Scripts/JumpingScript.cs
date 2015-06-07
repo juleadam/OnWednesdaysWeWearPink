349769321d494377c74c865eb483f61783228cd0
Julia Adamsson <julia.adamsson@gmail.om>
2015-06-07 16:35:04 +0200

    using sprites instead of cubes. character jumps on touch

--- OnWednesdaysWeWearPink/Assets/Scripts/JumpingScript.cs
+++ OnWednesdaysWeWearPink/Assets/Scripts/JumpingScript.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class JumpingScript : MonoBehaviour {
 
+	Rigidbody2D _rigidBody;
+
 	void Start () {
 		InputRecogniser.OnTouch += Jump;
+		_rigidBody = this.GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
@@ -13,6 +16,7 @@ public class JumpingScript : MonoBehaviour {
 
 	void Jump() {
 		Debug.Log ("jump!");
+		_rigidBody.AddForce (new Vector2(0, 20), ForceMode2D.Impulse);
 	}
 
 }
