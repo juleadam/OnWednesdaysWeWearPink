8f66cab80e2e5c64ef9c55e4396a6df2a3c7097e
Julia Adamsson <julia.adamsson@gmail.om>
2015-06-07 14:48:53 +0200

    Added input recogniser

---
+++ OnWednesdaysWeWearPink/Assets/Scripts/InputRecogniser.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputRecogniser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInput ();
+	}
+
+	void CheckInput () {
+		
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("jump!");
+		}
+	}
+}
