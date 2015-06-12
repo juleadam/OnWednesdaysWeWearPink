60016fd7909e70d56bb5aff5486f0a03fe01921e
Erik Bylund <kirre.bylund@gmail.com>
2015-06-12 22:30:00 +0200

    Updates to PlatformSpawner
    Signed-off-by: Erik Bylund <kirre.bylund@gmail.com>

--- OnWednesdaysWeWearPink/Assets/Scripts/PlatformSpawner.cs
+++ OnWednesdaysWeWearPink/Assets/Scripts/PlatformSpawner.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class PlatformSpawner : MonoBehaviour {
 	public GameObject Platform;
 	void Start () {
+
+		var offset = -1f;
+
 		for (int i = 0; i < 10; i++) {
 			var platform = Instantiate(Platform, 
-			            new Vector2(transform.position.x, transform.position.y + (i*3)), 
+			            new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y + (i*3) + offset), 
 			            transform.rotation) as GameObject;
 			platform.GetComponent<PlatformMovement>().Speed = Random.Range(0.5f, 4f);
 			if(Random.Range(0f,1f) < 0.5f) {
