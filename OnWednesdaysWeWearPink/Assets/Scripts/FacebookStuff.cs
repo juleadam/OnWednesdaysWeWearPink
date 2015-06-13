600770932c20fd0569ae5e7e712a86ecdbd8f020
Julia Adamsson <julia.adamsson@gmail.om>
2015-06-13 16:07:34 +0200

    rest of menu scene

---
+++ OnWednesdaysWeWearPink/Assets/Scripts/FacebookStuff.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FacebookStuff : MonoBehaviour {
+
+	void Start () {
+
+		if (FB.IsLoggedIn) {
+			GetComponent<Text> ().text = FB.UserId;
+		}
+	}
+
+}
