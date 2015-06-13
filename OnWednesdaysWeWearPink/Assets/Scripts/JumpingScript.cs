6ebbd403d0295f01e9e603efb21b461107f70d4b
Julia Adamsson <julia.adamsson@gmail.om>
2015-06-13 11:12:42 +0200

    unsubscribing events

--- OnWednesdaysWeWearPink/Assets/Scripts/JumpingScript.cs
+++ OnWednesdaysWeWearPink/Assets/Scripts/JumpingScript.cs
@@ -1,69 +1,55 @@
 using UnityEngine;
-using System.Collections;
 
 public class JumpingScript : MonoBehaviour {
 
-	Rigidbody2D _rigidBody;
 	public Collider2D Ground;
 	public bool IsGrounded;
 	public Transform groundCheck;
-	float _groundRadius = 0.2f;
 	public LayerMask whatIsGround;
 	public float MaxJumpForce;
 
-
+	Rigidbody2D _rigidBody;
+	const float _groundRadius = 0.2f;
 	float _chargeLevel = 0;
-	float _chargeSpeed = 2f;
-	float _jumpVelocity = 25f;
+	const float _chargeSpeed = 2f;
+	const float _jumpVelocity = 25f;
 
 	void Start () {
 		InputRecogniser.OnTouchDown += Charge;
 		InputRecogniser.OnTouchRelease += Jump;
-		Debug.Log (_rigidBody);
-		Debug.Log (this);
-		_rigidBody = this.GetComponent<Rigidbody2D> ();
-		Debug.Log (_rigidBody);
-
-
+		_rigidBody = GetComponent<Rigidbody2D> ();
 		IsGrounded = false;
 	}
 
-	void Update () {
-	
-	}
-
 	void FixedUpdate() {
-
 		IsGrounded = Physics2D.OverlapCircle (groundCheck.position, _groundRadius, whatIsGround);
 		Ground = Physics2D.OverlapCircle (groundCheck.position, _groundRadius, whatIsGround);
-
 	}
 
 	void Jump() {
-		if (_rigidBody == null) {
-			Debug.Log (_rigidBody);
-			Debug.Log (this);
-			_rigidBody = this.GetComponent<Rigidbody2D> ();
-			Debug.Log (_rigidBody);
-		}
-		Debug.Log ("Jump chargelevel" + _chargeLevel);
-
+		
 		var jumpForce = _jumpVelocity * _chargeLevel;
 		if (jumpForce > MaxJumpForce) {
 			jumpForce = MaxJumpForce;
 		}
-		Debug.Log (_rigidBody);
+
 		_rigidBody.AddForce (new Vector2(0, jumpForce), ForceMode2D.Impulse);
 		_chargeLevel = 0;
 	}
 
 	void Charge() {
-		
 		if (IsGrounded) {
-			//Debug.Log ("charge");
 			_chargeLevel += Time.deltaTime * _chargeSpeed;
-			//Debug.Log ("chargelevel: " + _chargeLevel);
 		}
+	}
+
+	void OnDestroy() {
+		UnsubscribeEvents ();
+	}
+
+	void UnsubscribeEvents() {
+		InputRecogniser.OnTouchDown -= Charge;
+		InputRecogniser.OnTouchRelease -= Jump;
 	}
 
 }
